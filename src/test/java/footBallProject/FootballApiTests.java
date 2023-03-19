13058b985eed6ced85beb0230a2af6e9d33001f7
AnanasName <martin25102002@gmail.com>
2023-03-19 17:19:43 +0300

    getDateFounded and getFirstTeanName added

--- src/test/java/footBallProject/FootballApiTests.java
+++ src/test/java/footBallProject/FootballApiTests.java
@@ -4,6 +4,7 @@ import footBallProject.config.FootballConfig;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class FootballApiTests extends FootballConfig {
 
@@ -13,5 +14,23 @@ public class FootballApiTests extends FootballConfig {
                 .queryParam("areas", 2255).
         when()
                 .get("areas");
+    }
+
+    @Test
+    public void getDateFounded(){
+        given().
+        when()
+                .get("teams/57").
+        then()
+                .body("founded", equalTo(1886));
+    }
+
+    @Test
+    public void getFirstTeamName(){
+        given().
+        when()
+                .get("competitions/").
+        then().
+                body("competitions[0].name", equalTo("Campeonato Brasileiro Série A"));
     }
 }
