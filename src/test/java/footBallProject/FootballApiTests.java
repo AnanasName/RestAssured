db81fc650cecc5bdbbb443db4381d0d450c70147
AnanasName <martin25102002@gmail.com>
2023-03-19 18:41:50 +0300

    Header and JSON Response tests added

--- src/test/java/footBallProject/FootballApiTests.java
+++ src/test/java/footBallProject/FootballApiTests.java
@@ -1,8 +1,12 @@
 package footBallProject;
 
 import footBallProject.config.FootballConfig;
+import io.restassured.http.ContentType;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
 import org.junit.Test;
 
+import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -32,5 +36,41 @@ public class FootballApiTests extends FootballConfig {
                 .get("competitions/").
         then().
                 body("competitions[0].name", equalTo("Campeonato Brasileiro Série A"));
+    }
+
+    @Test
+    public void getAllTeamData(){
+        String responseBody = get("teams/57").asString();
+        System.out.println(responseBody);
+    }
+
+    @Test
+    public void getAllTeamData_DoCheckFirst(){
+        Response response =
+                given().
+                when()
+                        .get("teams/57").
+                then()
+                        .contentType(ContentType.JSON)
+                        .extract().response();
+
+        String jsonReponseAsString = response.asString();
+    }
+
+    @Test
+    public void extractHeaders(){
+        Response response =
+                given().
+                when().
+                        get("teams/57").
+                then()
+                        .contentType(ContentType.JSON)
+                        .extract().response();
+
+        Headers headers = response.getHeaders();
+
+        String contentType = response.getHeader("Content-Type");
+
+        System.out.println(contentType);
     }
 }
