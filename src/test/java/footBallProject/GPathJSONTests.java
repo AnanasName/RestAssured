bb1ce79b5274ca442674e0877d34f2f7cdfac3c7
AnanasName <martin25102002@gmail.com>
2023-03-22 01:23:52 +0300

    extract tests added

--- src/test/java/footBallProject/GPathJSONTests.java
+++ src/test/java/footBallProject/GPathJSONTests.java
@@ -4,6 +4,7 @@ import footBallProject.config.FootballConfig;
 import io.restassured.response.Response;
 import org.junit.Test;
 
+import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.get;
@@ -18,5 +19,19 @@ public class GPathJSONTests extends FootballConfig {
         Map<String, ?> allTeamDataForSingleTeam = response.path("competitions.find { it.name == 'Serie A' }");
 
         System.out.println("Map of Team Data = " + allTeamDataForSingleTeam);
+    }
+
+    @Test
+    public void extractSingleValueWithFind(){
+        Response response = get("teams/57");
+        String certainPlayer = response.path("squad.find {it.shirtNumber == 23}.name");
+        System.out.println("Name of player = " + certainPlayer);
+    }
+
+    @Test
+    public void extractListOfValuesWithFindAll(){
+        Response response = get("teams/57");
+        List<String> playerNames = response.path("squad.findAll {it.shirtNumber >= 23}.name");
+        System.out.println("List of players" + playerNames);
     }
 }
