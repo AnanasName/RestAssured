933882a87f15f5f8e02cc55adbd674d6176355b3
Nikitin Nikita <martin25102002@gmail.com>
2023-03-29 14:24:08 +0300

    Lombok added

--- src/test/java/petProject/pojo/Category.java
+++ src/test/java/petProject/pojo/Category.java
@@ -1,33 +1,16 @@
 
 package petProject.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
 
     private Long id;
     private String name;
-
-    public Category() {
-    }
-
-    public Category(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
 }
--- src/test/java/petProject/pojo/Pet.java
+++ src/test/java/petProject/pojo/Pet.java
@@ -1,7 +1,14 @@
 package petProject.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Pet {
     private Category category;
 
@@ -14,66 +21,5 @@ public class Pet {
     private String status;
 
     private List<Tag> tags;
-
-    public Pet() {
-
-    }
-
-    public Pet(Category category, Long id, String name, List<String> photoUrls, String status, List<Tag> tags) {
-        this.category = category;
-        this.id = id;
-        this.name = name;
-        this.photoUrls = photoUrls;
-        this.status = status;
-        this.tags = tags;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<String> getPhotoUrls() {
-        return photoUrls;
-    }
-
-    public void setPhotoUrls(List<String> photoUrls) {
-        this.photoUrls = photoUrls;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public List<Tag> getTags() {
-        return tags;
-    }
-
-    public void setTags(List<Tag> tags) {
-        this.tags = tags;
-    }
 
 }
