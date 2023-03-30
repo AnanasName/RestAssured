bd064b63ebc1bda3a47b1ed0998763d689b5030e
Nikitin Nikita <martin25102002@gmail.com>
2023-03-30 14:08:20 +0300

    GetOrderByID test added

--- src/test/java/petStoreProject/store/StoreTest.java
+++ src/test/java/petStoreProject/store/StoreTest.java
@@ -1,5 +1,6 @@
 package petStoreProject.store;
 
+import io.restassured.response.Response;
 import org.junit.Test;
 import petStoreProject.store.config.StoreConfig;
 import petStoreProject.store.config.StoreEndpoints;
@@ -11,6 +12,7 @@ import java.time.ZoneOffset;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
 
 public class StoreTest extends StoreConfig {
     @Test
@@ -30,5 +32,19 @@ public class StoreTest extends StoreConfig {
                 post(StoreEndpoints.STORE_ORDER)
                 .then()
                 .body("id", equalTo(orderId));
+    }
+
+    @Test
+    public void getOrderByID(){
+        int orderId = 213;
+
+        Response response = given()
+                .pathParam("orderId", orderId)
+                .when()
+                .get(StoreEndpoints.STORE_ORDER_BY_ID);
+
+        Order order = response.as(Order.class);
+
+        assertEquals(orderId, order.getId());
     }
 }
